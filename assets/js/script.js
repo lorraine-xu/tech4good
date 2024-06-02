3c300d637cb879448a550d0774c9256ce6468316
yixuanwong <yixuanwong@ucsb.edu>
2024-06-02 12:45:48 -0700

    Added carousel and buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Website loaded!');
     // Future interactive scripts can go here.
+
+    new Vue({
+        el: '#app',
+        data: {
+            items: [
+                { src: 'assets/images/earth-globe.png', alt: 'Smiling Earth', width: '300' },
+                null,
+                null,
+                null,
+                null,
+                null
+            ]
+        }
+    });
 });
