e601bd8055b1ec25438eda7397bada3cdb4508d5
Simon Zhang <zhangshi0512@gmail.com>
2024-06-24 00:38:02 -0700

    postgres and flask works for register and login

--- config/db_config.py
+++ config/db_config.py
@@ -1,10 +1,10 @@
-from db_credentials import DB_USER, DB_PASSWORD
+from .db_credentials import DB_USER, DB_PASSWORD
 
 
 DB_CONFIG = {
     'host': 'localhost',
     'port': '5432',
-    'database': 'test_db',
+    'database': 'tech4good',
     'user': DB_USER,
     'password': DB_PASSWORD
 }
